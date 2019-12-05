79f683ce9e7c7d8d6ce931d23c54d80ff9d11954
sneha.govindharajan <snehag2205@gmail.com>
2019-12-05 17:56:12 +0530

    UI Commit after adding factorial function

--- app.py
+++ app.py
@@ -30,6 +30,19 @@ def x_power_y_response():
     return render_template("x_power_y_res.html", result=x_power_y_value)
 
 
+@app.route('/factorial_request')
+def factorial_request():
+    return render_template("factorial_req.html")
+
+
+@app.route('/factorial_response', methods=["POST"])
+def factorial_response():
+    f_value = request.form.get("fact_value")
+    flask_obj = ScientificCalc()
+    fact_result = flask_obj.factorial(f_value)
+    return render_template("factorial_res.html", result=fact_result)
+
+
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
     app.run(debug=True)
