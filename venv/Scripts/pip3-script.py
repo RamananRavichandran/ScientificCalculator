3fc552f714320c45f12922447b000100de6b620c
sneha.govindharajan <snehag2205@gmail.com>
2019-12-10 13:51:43 +0530

    dev1

--- venv/Scripts/pip3-script.py
+++ venv/Scripts/pip3-script.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-#!C:\Users\jayasree.suresh\PycharmProjects\ScientificCalculator\venv\Scripts\python.exe
-=======
-#!C:\Users\sneha.govindharajan\PycharmProjects\ScientificCalculator\venv\Scripts\python.exe
->>>>>>> 1fe08ad1d4c6ee6621a55d3d8b497113a87ee55c
-# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
-__requires__ = 'pip==19.0.3'
+#!C:\Users\karishma.murali\PycharmProjects\ScientificCalculator\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==10.0.1','console_scripts','pip3'
+__requires__ = 'pip==10.0.1'
 import re
 import sys
 from pkg_resources import load_entry_point
@@ -12,5 +8,5 @@ from pkg_resources import load_entry_point
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
     sys.exit(
-        load_entry_point('pip==19.0.3', 'console_scripts', 'pip3')()
+        load_entry_point('pip==10.0.1', 'console_scripts', 'pip3')()
     )
