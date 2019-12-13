b1f2390ef5c088e9247f115d0e36a4a47fd7cb02
sneha.govindharajan <snehag2205@gmail.com>
2019-12-13 15:07:05 +0530

    commit1

--- app.py
+++ app.py
@@ -259,31 +259,17 @@ def tanh_res():
     result = c1.calculate_tanh(angle)
     return render_template('tanh_response.html', value=result)
 
-
-# If we're running in stand alone mode, run the application
-if __name__ == '__main__':
-    app.run(debug=True)
-=======
-from flask import Flask, render_template, request
-from src.driver.scientific_calc import ScientificCalc
-
-app = Flask(__name__,template_folder="static")
-
-@app.route("/")
-def home():
-    return render_template('home.html')
-
 @app.route("/square_root_req")
 def square_root_req():
     return render_template('square_root.html')
 
 @app.route("/square_root_res",methods=['POST'])
 def square_root_res():
-        value = request.form.get('input')
-        c1 = ScientificCalc(value)
-        result = c1.square_root(value)
+        value_sqrt = request.form.get('input')
+        c1 = ScientificCalc()
+        result = c1.square_root(value_sqrt)
         return render_template('square_root_response.html',value1=result)
 
-if __name__=='__main__':
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
     app.run(debug=True)
->>>>>>> 45c6c24bdc0ef2a064eb576446014c3c8732670d
